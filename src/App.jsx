5f9022ff82439b26b2a909827e4ee39296e1868f
Emil <e.garaev.dg@gmail.com>
2025-01-26 18:57:48 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import {useState, useEffect} from "react";
+import hero from "./assets/hero.png"
+import Search from "./components/Search.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+const API_BASE_URL = 'http://api.themoviedb.org/3';
+const API_KEY =  import.meta.env.VITE_TMDB_API_KEY;
+const App = () => {
+
+const [searchTerm, setSearchTerm] = useState('');
+
+useEffect(() => {
+
+}, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <main>
+          <div className="pattern">
+              <div className="wrapper">
+                  <header>
+                      <img src={hero} alt="hero banner"/>
+                      <h1>Find <span className="text-gradient">Movies</span>
+                          You&#39;ll Enjoy Without the Hassle </h1>
+                  </header>
+                  <Search setSearchTerm={setSearchTerm} searchTerm={searchTerm} />
+              </div>
+          </div>
+      </main>
   )
 }
 
